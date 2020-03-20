a7fae628d5c8af2c92b3333d6d53145fdc964d4e
Egor Baryshnikov <egor_baryshnikov@MBP-Egor.Dlink>
2020-03-20 18:29:27 +0300

    merged usage for photo and document

--- bot.py
+++ bot.py
@@ -19,10 +19,14 @@ def send_welcome(message):
                            Just upload the image (bounding box) as a photo.")
 
 # Model usage with photo
-@bot.message_handler(content_types=['photo'])
+@bot.message_handler(content_types=['photo', 'document'])
 def photo(message):
+    if message.content_type == 'photo':
+        fileID = message.photo[-1].file_id
+    else:
+        fileID = message.document.file_id
+
     # download the file
-    fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
     downloaded_file = bot.download_file(file_info.file_path)
 
@@ -35,27 +39,6 @@ def photo(message):
 
     # save the file
     with open(bot_working_dir+"image.jpg", 'wb') as new_file:
-        new_file.write(downloaded_file)
-
-    # make prediction from the saved file
-    img = torch.tensor(imread(bot_working_dir+"image.jpg") / 255, dtype=torch.float32)
-    prediction_dict = classify_img(img.permute(2, 0, 1))
-
-    # choose best class and send to user
-    sorted_prediction = sorted(prediction_dict.items(), key=lambda x: x[1], reverse=True)
-    best_prediction = sorted_prediction[0]
-    bot.reply_to(message, '{}'.format(best_prediction[0]))
-
-# Model usage with documents
-@bot.message_handler(content_types=['document'])
-def photo(message):
-    # download the file
-    fileID = message.document.file_id
-    file_info = bot.get_file(fileID)
-    downloaded_file = bot.download_file(file_info.file_path)
-
-    # save the file
-    with open(bot_working_dir + "image.jpg", 'wb') as new_file:
         new_file.write(downloaded_file)
 
     # make prediction from the saved file
